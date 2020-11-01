7d6b124550e10a078a609913665e116d0ad75424
Artem <44427870+arteemk@users.noreply.github.com>
2020-11-02 02:52:12 +0700

    first commit

---
+++ my/assets/js/script.js
@@ -0,0 +1,69 @@
+const corsAnywherePrefix = 'https://cors-anywhere.herokuapp.com/';
+const searchUrlPrefix = 'https://store.playstation.com/chihiro-api/bucket-search/RU/ru/999/';
+
+const input = document.getElementById('input');
+const itemName = document.getElementById('item-name');
+
+async function request(url) {
+    const response = await fetch(url);
+    if (response.ok) {
+        return await response.json();
+    } else {
+        return false;
+    }
+}
+
+function formEntities(results) {
+    results.forEach(result => {
+        if (result['top_category'] !== 'promotional_material') {
+            const item = {};
+
+            if (result['top_category'] === 'downloadable_game') {
+                item.imageUrl =
+                    `https://store.playstation.com/store/api/chihiro/00_09_000/titlecontainer/RU/ru/999/${
+                        result['id'].split('-')[1]
+                    }/image?w=236&h=236`;
+
+            } else {
+                request(result['url'])
+                    .then(data => {
+                        if (data !== false) {
+                            item.imageUrl = data['images'][0]['url'];
+                        }
+                    });
+            }
+
+            item.id = result['id'];
+            item.name = result['name'];
+            item.displayDefaultPrice = result['default_sku']['display_price'];
+
+            if (result['skus'][0]['rewards'].length > 0) {
+                item.displayDiscountPrice = result['skus'][0]['rewards'][0]['display_price'];
+                item.discountPercent = result['skus'][0]['rewards'][0]['discount'];
+            }
+
+            item.platforms = result['playable_platform'];
+            item.releaseDate = result['release_date'];
+
+            console.log(item);
+        }
+    });
+}
+
+function search(name) {
+    request(corsAnywherePrefix + searchUrlPrefix + encodeURI(name))
+        .then(data => {
+            if (data !== false) {
+                formEntities(data['categories']['games']['links']);
+            }
+        });
+
+    return false;
+}
+
+/*
+* если top_category == 'downloadable_game', то берем id.spit('-')[1] и подставляем в imageUrl
+* если top_category != 'downloadable_game', то делаем еще запрос по полю url и выбираем images[0].url
+*
+* */
+
